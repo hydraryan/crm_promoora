69ccda623da0507e52f1127aa2bf5210867dd691
hydraryan <r.aryanraj96@gmail.com>
2026-03-23 19:09:22 +0530

    Deploy fix: compile backend to server-dist for Vercel function

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import app from '../server/app.js'
-import { connectDatabase } from '../server/config/db.js'
+import app from '../server-dist/app.js'
+import { connectDatabase } from '../server-dist/config/db.js'
 
 let dbReady: Promise<unknown> | null = null
 
